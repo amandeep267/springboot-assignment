2ed6d254919bacb3cfd0cf2b88652fba3b45a491
amandeep267 <97876877+amandeep267@users.noreply.github.com>
2022-04-21 12:30:09 +0530

    Update StudentBookService.java

--- src/main/java/com/springbootProject/demo/service/StudentBookService.java
+++ src/main/java/com/springbootProject/demo/service/StudentBookService.java
@@ -7,8 +7,7 @@ import java.util.List;
 
 public interface StudentBookService {
 
-
-    List<StudentBookList> findAllByUserName(String username);
+   List<StudentBookList> findAllByUserName(String username);
    public void save(StudentBookList theBook);
    public void deleteById(int theId);
 }
