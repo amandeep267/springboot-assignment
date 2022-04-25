a0f05ad6d31d67e8cf5312f432b477bf4409a425
amandeep267 <97876877+amandeep267@users.noreply.github.com>
2022-04-26 00:22:29 +0530

    Update LoginController.java

--- src/main/java/com/springbootProject/demo/controller/LoginController.java
+++ src/main/java/com/springbootProject/demo/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.springbootProject.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,10 @@ import java.io.IOException;
 
 @Controller
 public class LoginController {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+
+
     @RequestMapping("/login")
     public String login()
     {
@@ -27,16 +33,23 @@ public class LoginController {
 
         if(role.contains("ROLE_ADMIN")){
             response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/admin/books"));
+            logger.info("User is ADMIN");
+
         }
         else if(role.contains("ROLE_STUDENT")) {
+            logger.info("User is student");
+
             response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/student/books"));
+
         }
     }
     @GetMapping("/access-denied")
     public String renderAccessDeniedPage() {
+        logger.info("Operation  access is denied");
 
         return "accessDenied";
     }
 
 }
+
 
